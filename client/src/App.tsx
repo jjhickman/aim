171398cef213a27ee7f14cfcb2c4b03a818da4ed
Josh Hickman <me@jjhickman.com>
2025-05-08 02:29:40 -0400

    debugging client build, finished implementation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
 import CanvasMap from './CanvasMap'
 import './App.css'
 
 function App() {
-  const [count, setCount] = React.useState(0)
 
   return (
     <>
@@ -11,12 +9,7 @@ function App() {
         <h1>AIM Machine Map</h1>
       </div>
       <div className="card">
-        <button onClick={() => setCount(c => c + 1)}>
-          Add machine
-        </button>
-      </div>
-      <div className="card">
-        <CanvasMap count={count} />
+        <CanvasMap />
       </div>
     </>
   )
--- client/src/CanvasMap.tsx
+++ client/src/CanvasMap.tsx
@@ -1,55 +1,78 @@
-import React from 'react'
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api'
+import { LatLngTuple } from "leaflet";
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
+import { MachineMapClient, Machine, MachineStreamRequest } from "./machine";
+import React from "react";
 
-const containerStyle = {
-  width: '400px',
-  height: '400px',
+const CENTER_COORDS: LatLngTuple = [
+  Number(process.env.CENTER_LAT ?? "71.0565"),
+  Number(process.env.CENTER_LON ?? "42.3555")
+];
+
+const ENVOY_URL = process.env.ENVOY_URL ?? "127.0.0.1:8000"
+const machineClient = new MachineMapClient(ENVOY_URL)
+
+export default function CanvasMap() {
+  const [ machines, setMachines ] = React.useState<Machine[]>([]);
+  const [ numMachines, setNumMachines ] = React.useState<number>(0);
+
+  const pause = React.useCallback(async (machine: Machine) => {
+    const request = new Machine(machine);
+    const response = await machineClient.Pause(request, {});
+    console.log(response);
+  }, []);
+
+  const unPause = React.useCallback(async (machine: Machine) => {
+    if (machine.fuel_level === 0) {
+      console.log(`Cannot move machine ${machine.id} - out of gas`)
+    }
+    const request = new Machine(machine);
+    const response = await machineClient.UnPause(request, {});
+    console.log(response);
+  }, []);
+
+  const streamMachines = React.useCallback(() => {
+    const request = new MachineStreamRequest();
+    const stream = machineClient.MachineStream(request, {});
+    stream.on('data', (response: Machine) => {
+      console.log(response);
+      if (numMachines > response.id) {
+        setMachines( prevState => [
+          ...prevState.slice(0, response.id),
+          {
+              ...prevState[response.id],
+              ...response,
+          } as Machine,
+          ...prevState.slice(response.id + 1)
+      ]);
+      } else if (numMachines === response.id) {
+        setMachines(prevState => [...prevState, response])
+      }
+    });
+    stream.on('end', () => {
+      // stream end signal
+      stream.cancel();
+    });
+  }, [numMachines]);
+
+  React.useEffect(() => {
+    setNumMachines(machines.length);
+  }, [machines]);
+
+  React.useEffect(() => {
+    streamMachines();
+  }, [streamMachines]);
+
+  return (
+    <MapContainer center={CENTER_COORDS} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {machines.length > 0 && machines.map((machine, index) => {
+        return (<div onClick={machine.is_paused ? () => unPause(machine) : () => pause(machine)}>
+          <Marker position={[machine.location.lat, machine.location.lon]} key={index.toString()}/>
+        </div>);
+      })}
+    </MapContainer>
+  );
 }
-
-// Boston!
-const center = {
-  lat: 42.356,
-  lng: 71.057,
-}
-
-type Props = {
-  count: number;
-};
-
-function CanvasMap({ count }: Props) {
-  const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: 'YOUR_API_KEY',
-  })
-
-  const [map, setMap] = React.useState(null)
-
-  const onLoad = React.useCallback(function callback(m) {
-    // This is just an example of getting and using the map instance!!! don't just blindly copy!
-    const bounds = new window.google.maps.LatLngBounds(center)
-    m.fitBounds(bounds)
-
-    setMap(m)
-  }, [])
-
-  const onUnmount = React.useCallback(function callback(m) {
-    setMap(null)
-  }, [])
-
-  return isLoaded ? (
-    <GoogleMap
-      mapContainerStyle={containerStyle}
-      center={center}
-      zoom={10}
-      onLoad={onLoad}
-      onUnmount={onUnmount}
-    >
-      {/* Child components, such as markers, info windows, etc. */}
-      <></>
-    </GoogleMap>
-  ) : (
-    <></>
-  )
-}
-
-export default React.memo(CanvasMap)
